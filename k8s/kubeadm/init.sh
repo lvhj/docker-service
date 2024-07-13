ed139d80d096d50c3f68bc5712d5b37cb38c02fe
lvhj <lvhjvip@gmail.com>
2024-07-13 15:50:06 +0800

    feat: add kubesphere service

--- k8s/kubeadm/init.sh
+++ k8s/kubeadm/init.sh
@@ -1,5 +1,5 @@
 kubeadm init
 --apiserver-advertise-address=172.23.120.198
 --image-repository=registry.aliyuncs.com/google_containers
---pod-network-cidr=10.244.0.0/16
+--pod-network-cidr=10.244.0.0/24
 
